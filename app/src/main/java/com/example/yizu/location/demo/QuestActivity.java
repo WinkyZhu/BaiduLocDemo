d5539d67c02090cde70cd222cc2fe5f9c1206b8e
zhuxiangqing <zhuxiangqing_king@163.com>
2017-11-19 22:28:03 +0800

    init

---
+++ app/src/main/java/com/example/yizu/location/demo/QuestActivity.java
@@ -0,0 +1,19 @@
+package com.example.yizu.location.demo;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.yizu.R;
+
+/**
+ * 常见问题
+ */
+public class QuestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quest_activity);
+    }
+}
